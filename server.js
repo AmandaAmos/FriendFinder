a6fe240aa735f53f5e1c21d0b79814a222453136
Amanda Amos <59996872+AmandaAmos@users.noreply.github.com>
2020-04-26 14:46:29 -0700

    setting up server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 //server initialization and set up
+//Dependencies
+
+var express = require("express");
+var path = require("path");
+
+//sets up Express APP
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//express to handle data 
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
